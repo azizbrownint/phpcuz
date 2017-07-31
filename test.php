926ac01af910f7fb39456f34423a6eba6dfe7304
azizbrownint <30603616+azizbrownint@users.noreply.github.com>
2017-08-01 03:32:17 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+	$king = 1060;
+		echo $king . " ";
+
+	if ($king <150){
+		echo "little king....";
+	}
+	else {
+		echo "Welcome sovereign.";
+	}
+
+//do whatever you want here man. test out what you learn!
+>
